9c9d16c8916177f98e4383aeb4ffc0328785d416
Simon Svensson <simon.svensson@lund.ntig.se>
2017-08-29 09:58:25 +0200

    Reset board

--- MineSweeper/MineSweeperLogic/MineSweeperGame.cs
+++ MineSweeper/MineSweeperLogic/MineSweeperGame.cs
@@ -9,14 +9,14 @@ namespace MineSweeperLogic
 {
     public class MineSweeperGame
     {
-        
+
         public MineSweeperGame(int sizeX, int sizeY, int nrOfMines, IServiceBus bus)
         {
             SizeX = sizeX;
             SizeY = sizeY;
             NumberOfMines = nrOfMines;
-            State = GameState.Playing;
-            //ResetBoard();
+            this.Bus = bus;
+            ResetBoard();
         }
 
         public int PosX { get; private set; }
@@ -25,10 +25,14 @@ namespace MineSweeperLogic
         public int SizeY { get; }
         public int NumberOfMines { get; }
         public GameState State { get; private set; }
+        private PositionInfo[,] Map;
+        private readonly IServiceBus Bus;
 
         public PositionInfo GetCoordinate(int x, int y)
         {
-            return null;
+            if (x < 0 || x > SizeX ||
+                y < 0 || y > SizeY) return null;
+            return Map[x, y];
         }
 
         public void FlagCoordinate()
@@ -41,6 +45,43 @@ namespace MineSweeperLogic
 
         public void ResetBoard()
         {
+            Map = new PositionInfo[SizeX, SizeY];
+
+            for (int x = 0; x < Map.GetLength(0); x++)
+            {
+                for (int y = 0; y < Map.GetLength(1); y++)
+                {
+                    PositionInfo info = new PositionInfo();
+                    info.X = x;
+                    info.Y = y;
+
+                    Map[x, y] = info;
+                }
+            }
+
+            PlaceMines();
+
+            for (int x = 0; x < Map.GetLength(0); x++)
+            {
+                for (int y = 0; y < Map.GetLength(1); y++)
+                {
+                    int numberOfMines = 0;
+                    for (int x2 = x - 1; x2 < x + 1; x2++)
+                    {
+                        for (int y2 = y - 1; y2 < y + 1; y2++)
+                        {
+                            if (x2 == x && y2 == y) // ignore middle position
+                                continue;
+                            PositionInfo info = GetCoordinate(x2, y2);
+                            if (info != null && info.HasMine)
+                                numberOfMines++;
+                        }
+                    }
+                    GetCoordinate(x, y).NrOfNeighbours = numberOfMines;
+                }
+            }
+
+            State = GameState.Playing;
         }
 
         public void DrawBoard()
@@ -67,5 +108,24 @@ namespace MineSweeperLogic
 
         #endregion
 
+        private void PlaceMines()
+        {
+            int numberOfMinesBeenPlaced = 0;
+
+            while (numberOfMinesBeenPlaced < NumberOfMines)
+            {
+                int x = Bus.Next(SizeX);
+                int y = Bus.Next(SizeY);
+
+                PositionInfo positionToPlaceMine = GetCoordinate(x, y);
+
+                if (positionToPlaceMine.HasMine)
+                    continue;
+
+                positionToPlaceMine.HasMine = true;
+
+                numberOfMinesBeenPlaced++;
+            }
+        }
     }
 }
